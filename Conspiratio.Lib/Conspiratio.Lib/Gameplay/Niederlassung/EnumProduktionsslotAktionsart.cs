ba34d07651c55158fd70b062be45c264be648e5c
Sir Toby <baldur_angbant@web.de>
2020-01-09 23:31:50 +0100

    Neuen Enumerator "EnumProduktionsslotAktionsart" hinzugefügt

---
+++ Conspiratio.Lib/Conspiratio.Lib/Gameplay/Niederlassung/EnumProduktionsslotAktionsart.cs
@@ -0,0 +1,28 @@
+﻿namespace Conspiratio.Lib.Gameplay.Niederlassung
+{
+    /// <summary>
+    /// Stellt alle vorhandenen Aktionsarten eines Produktionsslots in einer Stadt dar.
+    /// </summary>
+    public enum EnumProduktionsslotAktionsart
+    {
+        /// <summary>
+        /// Kein Auftrag ausgewählt
+        /// </summary>
+        KeinAuftrag = 0,
+
+        /// <summary>
+        /// Ware herstellen
+        /// </summary>
+        Produzieren = 1,
+
+        /// <summary>
+        /// Ware in anderer Stadt einmalig verkaufen (exportieren)
+        /// </summary>
+        Verkaufen = 2,
+
+        /// <summary>
+        /// Ware in anderer Stadt permanent verkaufen (exportieren)
+        /// </summary>
+        PermanentVerkaufen = 3
+    }
+}
