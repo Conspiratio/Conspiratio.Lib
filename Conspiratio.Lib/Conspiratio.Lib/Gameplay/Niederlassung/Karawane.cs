58b0cc6ebb096e3aebfed912c5a049efb18def84
Sir Toby <baldur_angbant@web.de>
2020-01-25 14:27:13 +0100

    Die Klassen "Werbegeschenk" und "Karawane" hinzugefügt

---
+++ Conspiratio.Lib/Conspiratio.Lib/Gameplay/Niederlassung/Karawane.cs
@@ -0,0 +1,24 @@
+﻿namespace Conspiratio.Lib.Gameplay.Niederlassung
+{
+    public class Karawane
+    {
+        public int ID { get; }
+        public int Fixpreis { get; }
+        public int Sicherheit { get; }
+        public int Verlaesslichkeit { get; }
+        public string Beschreibung { get; }
+        public int PreisProStueck { get; }
+        public int Kapazitaet { get; }
+
+        public Karawane(int id, int fixpreis, int preisProStueck, int kapazitaet, int sicherheit, int verlaesslichkeit, string beschreibung)
+        {
+            ID = id;
+            Fixpreis = fixpreis;
+            PreisProStueck = preisProStueck;
+            Kapazitaet = kapazitaet;
+            Sicherheit = sicherheit;
+            Verlaesslichkeit = verlaesslichkeit;
+            Beschreibung = beschreibung;
+        }
+    }
+}
