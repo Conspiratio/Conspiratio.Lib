b608e15b095085238cd6af4bba19030dc8d79648
Sir Toby <baldur_angbant@web.de>
2020-01-26 14:48:13 +0100

    Klasse "Strafen" in "Strafe" umbenannt und Interface hinzugefügt, Klasse "Privileg" mit Interface hinzugefügt

---
+++ Conspiratio.Lib/Conspiratio.Lib/Gameplay/Justiz/Strafe.cs
@@ -0,0 +1,14 @@
+﻿namespace Conspiratio.Lib.Gameplay.Justiz
+{
+    public abstract class Strafe : IStrafe
+    {
+        public string Name { get; }
+
+        protected Strafe(string name)
+        {
+            Name = name;
+        }
+
+        public abstract string StrafeExecute(int opferID);
+    }
+}
