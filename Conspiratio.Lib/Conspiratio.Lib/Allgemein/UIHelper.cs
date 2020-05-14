5e451b2175c32e095ac919a291b52d2177192ed3
Sir Toby <baldur_angbant@web.de>
2020-05-14 13:55:33 +0200

    Changelog erweitert; neue Klasse "UIHelper" hinzugefügt und in der Klasse "SW" als Singleton untergebracht; neues Interface "IJaNeinFrage" für die Dialogbox hinzugefügt

---
+++ Conspiratio.Lib/Conspiratio.Lib/Allgemein/UIHelper.cs
@@ -0,0 +1,12 @@
+﻿namespace Conspiratio.Lib.Allgemein
+{
+    public class UIHelper
+    {
+        public IJaNeinFrage JaNeinFrage { get; private set; }
+
+        public void Initialisieren(IJaNeinFrage jaNeinFrage)
+        {
+            JaNeinFrage = jaNeinFrage;
+        }
+    }
+}
--- Conspiratio.Lib/Conspiratio.Lib/Gameplay/Spielwelt/SW.cs
+++ Conspiratio.Lib/Conspiratio.Lib/Gameplay/Spielwelt/SW.cs
@@ -1,4 +1,6 @@
-﻿namespace Conspiratio.Lib.Gameplay.Spielwelt
+﻿using Conspiratio.Lib.Allgemein;
+
+namespace Conspiratio.Lib.Gameplay.Spielwelt
 {
     /// <summary>
     /// Spielwelt, die alle untergeordneten Objekte von statischen und dynamischen Daten enthält.
@@ -6,6 +8,7 @@
     public static class SW
     {
         private static StatischeSpieldaten _statisch = null;
+        private static UIHelper _uiHelper = null;
 
         public static StatischeSpieldaten Statisch
         {
@@ -15,6 +18,17 @@
                     _statisch = new StatischeSpieldaten();
 
                 return _statisch;
+            }
+        }
+
+        public static UIHelper UI
+        {
+            get
+            {
+                if (_uiHelper == null)
+                    _uiHelper = new UIHelper();
+
+                return _uiHelper;
             }
         }
     }
