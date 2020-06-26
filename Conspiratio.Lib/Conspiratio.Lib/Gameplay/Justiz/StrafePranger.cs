7a96ab7638b6acf656b516cc8d94c44987d2ae01
Sir Toby <baldur_angbant@web.de>
2020-06-26 20:07:24 +0200

    Neue Klassen und Enum aus dem WinForms Projekt hinzugefügt

---
+++ Conspiratio.Lib/Conspiratio.Lib/Gameplay/Justiz/StrafePranger.cs
@@ -0,0 +1,25 @@
+﻿using Conspiratio.Lib.Gameplay.Spielwelt;
+
+namespace Conspiratio.Lib.Gameplay.Justiz
+{
+    public class StrafePranger : Strafe
+    {
+        public StrafePranger() : base("Pranger")
+        {
+        }
+
+        public override string StrafeExecute(int opferID)
+        {
+            if (opferID < SW.Statisch.GetMinKIID())
+            {
+                SW.Dynamisch.GetHumWithID(opferID).ErhoehePermaAnsehen(-50);
+            }
+            else
+            {
+                SW.Dynamisch.GetSpWithID(opferID).ErhoeheAnsehen(-50);
+            }
+
+            return SW.Dynamisch.GetSpWithID(opferID).GetName() + " muss einen Tag am Pranger verbringen.\nDas Ansehen von " + SW.Dynamisch.GetSpWithID(opferID).GetName() + " hat gelitten";
+        }
+    }
+}
