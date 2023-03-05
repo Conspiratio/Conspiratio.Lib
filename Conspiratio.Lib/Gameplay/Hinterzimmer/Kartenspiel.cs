178d5f2d69f2de3480c83e7e79e5362e248f4449
SirTobyB <baldur_angbant@web.de>
2023-03-05 20:22:24 +0100

    .editorconfig erstellt und mit der Auslagerung des 17 und 4 Kartenspiels begonnen (neue Klasse "Kartenspiel" hinzugefügt)

---
+++ Conspiratio.Lib/Gameplay/Hinterzimmer/Kartenspiel.cs
@@ -0,0 +1,36 @@
+﻿using Conspiratio.Lib.Gameplay.Spielwelt;
+
+namespace Conspiratio.Lib.Gameplay.Hinterzimmer
+{
+    public class Kartenspiel
+    {
+        public string GegnerName { get; private set; }
+        public string GegnerErSie { get; private set; }
+        public string GegnerSeinenIhren { get; private set; }
+        
+        public bool FindetKartenspielStatt => SW.Dynamisch.GetHumWithID(SW.Dynamisch.GetAktiverSpieler()).GetSpieltKartenGegenSpielerID() != 0;
+
+        public bool HatSpielerGenugTaler() => SW.Dynamisch.GetHumWithID(SW.Dynamisch.GetAktiverSpieler()).GetTaler() > SW.Statisch.GetKartenSpielenMinTaler();
+        
+        public void InitiiereKartenspielUndErmittleGegner()
+        {
+            if (SW.Dynamisch.GetGesetzX(4) > 0)
+                SW.Dynamisch.GetHumWithID(SW.Dynamisch.GetAktiverSpieler()).ErhoeheGesetzXUmEins(4);
+
+            var kiGegner = SW.Dynamisch.GetKIwithID(SW.Dynamisch.GetHumWithID(SW.Dynamisch.GetAktiverSpieler()).GetSpieltKartenGegenSpielerID());
+            
+            GegnerName = kiGegner.GetKompletterName();
+            
+            if (kiGegner.GetMaennlich())
+            {
+                GegnerErSie = "er";
+                GegnerSeinenIhren = "seinen";
+            }
+            else
+            {
+                GegnerErSie = "sie";
+                GegnerSeinenIhren = "ihren";
+            }
+        }
+    }
+}
