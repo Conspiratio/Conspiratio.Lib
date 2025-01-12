1c0227b1d4290070379370e3b4f68549431ec562
SirTobyB <baldur_angbant@web.de>
2025-01-12 08:14:51 +0100

    adds some refactoring of NewGameManager; adds resource files
    Resource files are for localization. Also increased version number of assembly.

--- Conspiratio.Lib/Allgemein/NewGameManager.cs
+++ Conspiratio.Lib/Allgemein/NewGameManager.cs
@@ -2,15 +2,33 @@
 
 using Conspiratio.Lib.Gameplay.Spielwelt;
 
+using JetBrains.Annotations;
+
 namespace Conspiratio.Lib.Allgemein
 {
     public class NewGameManager
     {
-        private string _savegamePath;
+        private readonly string _savegamePath;
 
         public NewGameManager(string savegamePath)
         {
             _savegamePath = savegamePath;
+        }
+        
+        [PublicAPI]
+        public int MaxLengthOfGameName
+        {
+            get
+            {
+                string savegamePathWithFilename = Path.Combine(_savegamePath, "_1600.dat");
+                int savegamePathLength = savegamePathWithFilename.Length;
+                int maxlength = 256 - savegamePathLength;
+
+                if (maxlength < 0) // fallback from settings (standard: 12), if savegame path is longer then 256 chars
+                    maxlength = SW.Statisch.GetMaxNameLength();
+
+                return maxlength;
+            }
         }
         
         public bool CreateNewGame(string name, int playerCount, bool cheating, bool showDeaths, bool testmode, out string error)
@@ -27,9 +45,10 @@ namespace Conspiratio.Lib.Allgemein
             return true;
         }
 
+        [PublicAPI]
         public bool ValidateName(string name, out string error)
         {
-            error = "Der Spielname muss aus mindestens drei Zeichen bestehen";
+            error = Resources.The_game_name_must_consist_of_at_least_three_characters;
             
             if (string.IsNullOrEmpty(name))
                 return false;
@@ -40,25 +59,19 @@ namespace Conspiratio.Lib.Allgemein
             error = "";
             return true;
         }
-
+        
+        [PublicAPI]
         public string SanitizeName(string name)
         {
-            string savegamePathWithFilename = Path.Combine(_savegamePath, "_1600.dat");
-            int savegamePathLength = savegamePathWithFilename.Length;
-            int maxlength = 256 - savegamePathLength;
-
-            if (maxlength < 0)  // fallback from settings (standard: 12), if savegame path is longer then 256 chars
-                maxlength = SW.Statisch.GetMaxNameLength();
-            
             name = RemoveInvalidChars(name);
             
-            if (name.Length > maxlength)
-                return name.Substring(0, maxlength);
+            if (name.Length > MaxLengthOfGameName)
+                return name.Substring(0, MaxLengthOfGameName);
 
             return name;
         }
         
-        private string RemoveInvalidChars(string filename)
+        private static string RemoveInvalidChars(string filename)
         {
             return string.Concat(filename.Split(Path.GetInvalidFileNameChars()));
         }
